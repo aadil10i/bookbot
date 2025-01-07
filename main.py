fec5bdff13acde71769178f34003a627950a084f
aadil10i <aadil3003@outlook.com>
2025-01-07 19:44:30 +0400

    char count

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ def main():
     book_path = "books/frankenstein.txt"
     text = open_book(book_path)
     num_words = get_num_words(text)
+
     print(num_words)
+    print(count_occurances(text))
+
+def open_book(path):
+    with open(path) as f:
+        return f.read()
 
 def get_num_words(text):
     total_count = 0
@@ -13,10 +19,18 @@ def get_num_words(text):
         
     return total_count
 
-def open_book(path):
-    with open(path) as f:
-        return f.read()
+def count_occurances(text):
+    char_count_dic = {}
+    for char in text:
+        lowered_case = char.lower()
 
+        for letter in lowered_case:
+            if letter in char_count_dic:
+                char_count_dic[letter] += 1
+            else:
+                char_count_dic[letter] = 1
+
+    return char_count_dic
 
 main()
 
