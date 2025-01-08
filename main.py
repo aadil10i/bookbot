1e0ac325914a9880ba51beaeaa0f6fd8f2391415
aadil10i <aadil3003@outlook.com>
2025-01-08 15:19:48 +0400

    finished occurance counter, alphabets only

--- main.py
+++ main.py
@@ -3,8 +3,12 @@ def main():
     text = open_book(book_path)
     num_words = get_num_words(text)
 
-    print(num_words)
-    print(count_occurances(text))
+    print(f"{num_words}: words found in the document \n")
+
+    occurence_list = count_occurances(text)
+    for key, value in occurence_list:
+        print(f"the {key} character was found {value} times \n")
+    print("--- End report ---")
 
 def open_book(path):
     with open(path) as f:
@@ -23,14 +27,18 @@ def count_occurances(text):
     char_count_dic = {}
     for char in text:
         lowered_case = char.lower()
+        # remove non alphabetical characters
+        if "a" <= lowered_case <= "z":
+            for letter in lowered_case:
+                if letter in char_count_dic:
+                    char_count_dic[letter] += 1
+                else:
+                    char_count_dic[letter] = 1
+            
+            char_count_list = list(char_count_dic.items())
+            char_count_list.sort(key=lambda item: item[1], reverse=True)
 
-        for letter in lowered_case:
-            if letter in char_count_dic:
-                char_count_dic[letter] += 1
-            else:
-                char_count_dic[letter] = 1
-
-    return char_count_dic
+    return char_count_list
 
 main()
 
